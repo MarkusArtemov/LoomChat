4305db4c4966677b6310d353a85c56d744f742d5
MarkusArtemov <Martemov16@gmail.com>
2025-01-24 18:57:13 +0100

    Hide file-view until a channel is selected

---
+++ Client/Plugins/IPlugin.cs
@@ -0,0 +1,26 @@
+﻿namespace De.Hsfl.LoomChat.Client.Plugins
+{
+    /// <summary>
+    /// Basisschnittstelle für jedes Plugin,
+    /// das zur Laufzeit geladen werden kann.
+    /// </summary>
+    public interface IPlugin
+    {
+        /// <summary>
+        /// Eindeutiger Name des Plugins, z.B. "Survey"
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Wird nach dem Laden aufgerufen.
+        /// Hier kann das Plugin interne Init-Logik durchführen.
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Beispiel-Methode zum Starten der eigentlichen Funktion
+        /// (z.B. Umfrage-Fenster öffnen).
+        /// </summary>
+        void Execute();
+    }
+}
